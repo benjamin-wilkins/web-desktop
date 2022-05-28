81a528534a0ff039883fe4537d994004f0d78c48
Benjamin Wilkins <62174924+benjamin-wilkins@users.noreply.github.com>
2022-05-28 17:12:43 +0100

    Fix icon size

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ desktop = document.getElementById("desktop");
 for (n=0; n<desktop_icons.length; n++) {
   button = `
   <button type="button" class="btn btn-outline-dark" onclick="windowButton("${desktop_icons[n]["page"]})"">
-    <span class="fs-2"><i class="bi-${desktop_icons[n]["icon"]}"></i></span>
+    <i class="bi-${desktop_icons[n]["icon"]}" style="font-size: 3rem"></i>
     <br>
     ${desktop_icons[n]["name"]}
   </button>
